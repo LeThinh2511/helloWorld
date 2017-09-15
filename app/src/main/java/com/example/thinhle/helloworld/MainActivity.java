afc31461f077d61f4400cfb95c45d680a81cbef3
LeThinh2511 <thinhlefrs@gmail.com>
2017-09-15 16:52:10 +0700

    datepicker

--- app/src/main/java/com/example/thinhle/helloworld/MainActivity.java
+++ app/src/main/java/com/example/thinhle/helloworld/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.thinhle.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.picker);
+        datePicker = (DatePicker) findViewById(R.id.datepicker);
+        timePicker = (TimePicker) findViewById(R.id.timepicker);
+//        spinner = (Spinner) findViewById(R.id.spin_language);
+//        List<String> list = new ArrayList<>();
+//        list.add("Java");
+//        list.add("Android");
+//        list.add("C++");
+//        list.add("Python");
+//        list.add("Pascal");
+//        list.add("C#");
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, list);
+//        arrayAdapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+//        spinner.setAdapter(arrayAdapter);
+//
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(MainActivity.this, spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
     }
 }
